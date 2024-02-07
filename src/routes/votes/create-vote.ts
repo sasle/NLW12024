8050999e04dbc15f01e0df3629690d92f53bbec5
sasle <sasle57@gmail.com>
2024-02-06 23:47:59 -0300

    create post vote route

--- src/routes/votes/create-vote.ts
+++ src/routes/votes/create-vote.ts
@@ -1,23 +1,60 @@
 import z from "zod";
-import prisma from "../../shared/prisma";
-import app from "../../http/server";
+import prisma from "../../shared/prisma.js";
+import app from "../../http/server.js";
+import { randomUUID } from "crypto";
 
 export default async function createVote() {
-  app.post("/polls/:pollId/vote", async (request, reply) => {
+  app.post("/polls/:pollId/votes", async (request, reply) => {
     const createOptionBody = z.object({
-      title: z.string(),
+      optionId: z.string().uuid(),
     });
 
     const createOptionHeader = z.object({
       pollId: z.string().uuid(),
     });
 
-    const { title } = createOptionBody.parse(request.body);
+    const { optionId } = createOptionBody.parse(request.body);
     const { pollId } = createOptionHeader.parse(request.params);
+
+    let { sessionId } = request.cookies;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.setCookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 30,
+        signed: true,
+        httpOnly: true,
+      });
+    }
+
+    const existingVoteForThisPoll = await prisma.vote.findUnique({
+      where: {
+        sessionId_pollId: {
+          pollId,
+          sessionId,
+        },
+      },
+    });
+
+    if (optionId == existingVoteForThisPoll?.pollOptionId)
+      return reply
+        .status(400)
+        .send({ message: "You have already voted for this option." });
+    else {
+      await prisma.vote.delete({
+        where: {
+          id: existingVoteForThisPoll?.id,
+        },
+      });
+    }
+
     return reply.status(201).send(
-      await prisma.pollOption.create({
+      await prisma.vote.create({
         data: {
-          title,
+          sessionId,
+          pollOptionId: optionId,
           pollId,
         },
       })
